8e6b654dfdf8eefac1cc5c74efb37af7341a4464
aidanling65 <2763417l@student.gla.ac.uk>
2024-02-08 17:42:45 +0000

    Chapter 7 Completed

--- rango/forms.py
+++ rango/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from rango.models import Page, Category
 
 class CategoryForm(forms.ModelForm):
-    name = forms.CharField(max_length=128, help_text="Please enter the category name.")
+    name = forms.CharField(max_length=Category.NAME_MAX_LENGTH, help_text="Please enter the category name.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
@@ -20,3 +20,13 @@ class PageForm(forms.ModelForm):
     class Meta:
         model = Page
         exclude=('category',)
+        
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+        
+        if url and not url.startswith('http://'):
+            url = f'http://{url}'
+            cleaned_data['url'] = url
+            
+        return cleaned_data
