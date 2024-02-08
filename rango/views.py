0a6ed6df948e1b81ee424509ed08326838cd31f9
unknown <2763417l@student.gla.ac.uk>
2024-02-08 00:54:06 +0000

    Categories implemented for index page

--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from models import Category
 
 def index(request):
-    context_dict = {'boldmessage' : 'Crunchy, creamy, cookie, candy, cupcake!'}
+    category_list = Category.objects.order_by('-likes')[:5]
+    
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+    
+    #Render the response and send it back
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
