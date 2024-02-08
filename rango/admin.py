4a4d0f5187c1935e5b59dbb93895ff64775be75c
unknown <2763417l@student.gla.ac.uk>
2024-02-08 00:38:27 +0000

    Completed chapter 4

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from rango.models import Category, Page
+from rango.pageadmin import PageAdmin
 
 admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
