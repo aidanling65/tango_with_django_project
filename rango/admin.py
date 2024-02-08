38a42715641d1688e229a747f2f386d3cac6cc18
unknown <2763417l@student.gla.ac.uk>
2024-02-08 01:47:17 +0000

    Category pages implemented

--- rango/admin.py
+++ rango/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from rango.models import Category, Page
 from rango.pageadmin import PageAdmin
 
-admin.site.register(Category)
 admin.site.register(Page, PageAdmin)
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+    
+admin.site.register(Category, CategoryAdmin)
