cc8e937c05abe5abef0baa67c910e00c5076c81e
aidanling65 <2763417l@student.gla.ac.uk>
2024-02-09 00:38:26 +0000

    Signup added

--- rango/admin.py
+++ rango/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from rango.models import Category, Page
 from rango.pageadmin import PageAdmin
+from rango.models import UserProfile
 
 admin.site.register(Page, PageAdmin)
 
@@ -8,3 +9,4 @@ class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)}
     
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(UserProfile)
